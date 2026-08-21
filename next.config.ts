12ad8e2936b836e1edb7476afac0532d4ac228b9
Dyad <newemaillol@tutamail.com>
2026-08-20 20:04:29 -0700

    (3 files changed)

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,17 @@ const nextConfig: NextConfig = {
   // Pin the workspace root to this project so Next.js does not pick up an
   // unrelated parent lockfile when inferring the root directory.
   outputFileTracingRoot: path.join(__dirname),
+  webpack: (config) => {
+    if (process.env.NODE_ENV === "development") {
+      config.module.rules.push({
+        test: /\.(jsx|tsx)$/,
+        exclude: /node_modules/,
+        enforce: "pre",
+        use: "@dyad-sh/nextjs-webpack-component-tagger",
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
